9f192d4a1f0224531d7c54aec4b264865d27e7bf
adrianacostep33 <adriana.coste@principal33.com>
2024-03-07 15:47:22 +0200

    114:  Add dynamic and conditional styling with styled components

--- 01-starting-project/src/components/AuthInputs.jsx
+++ 01-starting-project/src/components/AuthInputs.jsx
@@ -15,17 +15,18 @@ const Lable = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: #6b7280;
+  color: ${({ $invalid }) => ($invalid ? "#f87171" : "#6b7280")};
 `;
 
 const Input = styled.input`
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: #d1d5db;
-  color: #374151;
+  background-color: ${({ $invalid }) => ($invalid ? "#fed2d2" : "#d1d5db")};
+  color: ${({ $invalid }) => ($invalid ? "#f73f3f" : "#374151")};
   border: 1px solid transparent;
   border-radius: 0.25rem;
+  border-color: ${({ $invalid }) => ($invalid ? "#f73f3f" : "transparent")};
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `;
 
@@ -53,22 +54,18 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ControlContainer className="controls">
         <p>
-          <Lable className={`label ${emailNotValid ? "invalid" : ""}`}>
-            Email
-          </Lable>
+          <Lable $invalid={emailNotValid}>Email</Lable>
           <Input
             type="email"
-            className={emailNotValid ? "invalid" : undefined}
+            $invalid={emailNotValid}
             onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
-          <Lable className={`label ${emailNotValid ? "invalid" : ""}`}>
-            Password
-          </Lable>
+          <Lable $invalid={passwordNotValid}>Password</Lable>
           <Input
             type="password"
-            className={passwordNotValid ? "invalid" : undefined}
+            $invalid={passwordNotValid}
             onChange={(event) =>
               handleInputChange("password", event.target.value)
             }
